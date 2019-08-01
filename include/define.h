83795ad01429566531d05b44393d3e60f8bc81a5
rkeli <prokushevmaksim@gmail.com>
2019-08-02 00:03:29 +0300

    read +++

--- include/define.h
+++ include/define.h
@@ -5,14 +5,41 @@
 #ifndef WOLF3D_DEFINE_H
 #define WOLF3D_DEFINE_H
 
-# define QUIT (1 << 0)
+# define WOLF_QUIT (1 << 0)
 
-//# define WIN_HEIGHT 1315
-# define WIN_HEIGHT 1080
+/*
+** WINDOW
+*/
+
 # define WIN_WIDTH 1920
-//# define WIN_WIDTH 2560
+# define WIN_HEIGHT 1080
+
+/*
+** MAP
+*/
+
 # define MAP_PATH "../maps/map_01"
 # define MAP_PATH_TWO "/Users//Desktop/Wolf3D/maps/map_02"
+# define MAP_SCALE 30
+
+/*
+** PHYSICS
+*/
+
+# define PLAYER_SPEED 20
+# define ROTATION_SPEED 0.08f
+
+/*
+** RAYCAST
+*/
+
+# define COLUM 64
+# define FOV M_PI / 3
+
+/*
+** COLOR
+*/
+
 # define BLACK 0x00000000
 # define WHITE 0xFFFFFFFF
 # define GREEN 0x00FF0000
@@ -20,11 +47,11 @@
 # define DGRAY 0xa9a9a900
 # define BLUE 0x0000FF00
 # define RED 0xFF000000
-# define PLAYER_SPEED 20
-# define ROTATION_SPEED 0.08f
-# define MAP_SCALE 30
-# define FOV M_PI / 3
-# define COLUM 64
+
+/*
+** LOGIC
+*/
+
 # define TRUE 1
 # define FALSE 0
 

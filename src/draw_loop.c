b8603e8df375a4e755a787f6e463f4de0d71f2df
GodFlight <prokushevmaksim@gmail.com>
2019-08-19 16:28:43 +0300

    del unused functions

--- src/draw_loop.c
+++ src/draw_loop.c
@@ -6,26 +6,6 @@ void	sdl_put_pixel(t_sdl *sdl, int x, int y, int color)
 		sdl->data[x + y * sdl->win_w] = color;
 }
 
-void	draw_texture(t_rc_main *m, t_sdl *sdl)
-{
-	int		x;
-	int		y;
-	int		**tmp_arr;
-
-	y = 0;
-	tmp_arr = (m->walls[1]).texture3;
-	while (y < 64)
-	{
-		x = 0;
-		while (x < 64)
-		{
-			sdl->data[x + y * sdl->win_w] = tmp_arr[y][x];
-			x++;
-		}
-		y++;
-	}
-}
-
 int	draw_loop(void *m_v)
 {
 	t_rc_main	*m;
@@ -36,10 +16,8 @@ int	draw_loop(void *m_v)
 		SDL_LockMutex(m->sdl->mutex);
 		raycast_and_draw(m);
 		draw_objects(m);
-//		draw_texture(m, m->sdl);
 		SDL_SetRenderTarget(m->sdl->renderer, m->sdl->texture);
 		SDL_RenderClear(m->sdl->renderer);
-//		draw_interface(m);
 		SDL_UpdateTexture(m->sdl->texture, NULL, (void *)m->sdl->data, m->sdl->win_w * sizeof(int));
 		SDL_UnlockMutex(m->sdl->mutex);
 		SDL_SetRenderTarget(m->sdl->renderer, NULL);
